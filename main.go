1f92a61c1edfe70a863d6ce12ef48819597e15be
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-04-01 02:25:56 +0700

    membuat function helper

--- main.go
+++ main.go
@@ -5,18 +5,34 @@ import (
 )
 
 func FindMin(nums ...int) int {
-	return 0 // TODO: replace this
+	min := nums[0]
+	for _, num := range nums {
+		if num < min {
+			min = num
+		}
+	}
+
+	return min // TODO: replace this
 }
 
 func FindMax(nums ...int) int {
-	return 0 // TODO: replace this
+	max := nums[0]
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
 }
 
 func SumMinMax(nums ...int) int {
-	return 0 // TODO: replace this
+	min := FindMin(nums...)
+	max := FindMax(nums...)
+
+	return min + max
 }
 
 // gunakan untuk melakukan debug
 func main() {
-	fmt.Println(SumMinMax(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	fmt.Println(SumMinMax( 3, 1, 4, 5, 6, 7, 8, 9, 13))
 }
